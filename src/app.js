6c147d32cfee3ca8d287de4cf03e234741545058
kooshul <kooshul@gmail.com>
2015-04-11 15:05:47 -0400

    V1

--- src/app.js
+++ src/app.js
@@ -5,54 +5,20 @@
  */
 
 var UI = require('ui');
-var Vector2 = require('vector2');
 
+// Create a Card with title and subtitle
 var main = new UI.Card({
-  title: 'Pebble.js',
-  icon: 'images/menu_icon.png',
-  subtitle: 'Hello World!!',
-  body: 'Press any button.'
+  body:'Dinner Swipe until 9pm',
+  subtitle:'DBA: $582.30 Swipes: 3'
 });
 
+var hours = new UI.Card({
+  title:'Meal Periods',
+  subtitle:'Dinner: $582.30 Swipes: 3'
+});
+
+// Display the Card
 main.show();
+hours.show();
 
-main.on('click', 'up', function(e) {
-  var menu = new UI.Menu({
-    sections: [{
-      items: [{
-        title: 'Pebble.js',
-        icon: 'images/menu_icon.png',
-        subtitle: 'Can do Menus'
-      }, {
-        title: 'Second Item',
-        subtitle: 'Subtitle Text'
-      }]
-    }]
-  });
-  menu.on('select', function(e) {
-    console.log('Selected item #' + e.itemIndex + ' of section #' + e.sectionIndex);
-    console.log('The item is titled "' + e.item.title + '"');
-  });
-  menu.show();
-});
-
-main.on('click', 'select', function(e) {
-  var wind = new UI.Window();
-  var textfield = new UI.Text({
-    position: new Vector2(0, 50),
-    size: new Vector2(144, 30),
-    font: 'gothic-24-bold',
-    text: 'Text Anywhere!',
-    textAlign: 'center'
-  });
-  wind.add(textfield);
-  wind.show();
-});
-
-main.on('click', 'down', function(e) {
-  var card = new UI.Card();
-  card.title('A Card');
-  card.subtitle('Is a Window');
-  card.body('The simplest window type in Pebble.js.');
-  card.show();
-});
+/* https://www.dartmouth.edu/dining/locations/ */
